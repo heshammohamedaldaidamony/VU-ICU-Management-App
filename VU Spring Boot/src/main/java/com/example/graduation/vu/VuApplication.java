fb35b7497782f64d409fef474e2dcb59b71df3c7
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-06-29 13:58:02 +0300

    Spring Boot project - intial dependicies

---
+++ VU Spring Boot/src/main/java/com/example/graduation/vu/VuApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduation.vu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuApplication.class, args);
+	}
+
+}
---
+++ VU Spring Boot/src/test/java/com/example/graduation/vu/VuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduation.vu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
